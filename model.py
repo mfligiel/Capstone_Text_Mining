8f9d779e9825d1c6a6b631efb1a311ef64c3e146
mfligiel <76972796+mfligiel@users.noreply.github.com>
2021-04-12 14:01:04 -0500

    Update model.py
    Added some changes to the regressor; no longer has class balance and accuracy, but rather r2.

--- model.py
+++ model.py
@@ -22,7 +22,7 @@ def model(data, y_var, categorical = False, grid_search = False,\
 
     """
     if keywordvar:
-      data.drop(keywordvar, axis=1, inplace=True)
+      data = data.drop(keywordvar, axis=1)
     if categorical == True:
         data[y_var] = (df[y_var] > 0).astype(int)
         #from sklearn.model_selection import train_test_split
@@ -45,14 +45,13 @@ def model(data, y_var, categorical = False, grid_search = False,\
         X_train, X_test, y_train, y_test = train_test_split(\
             data.drop(y_var, axis=1), data[y_var], test_size=0.2, random_state=42)
         if grid_search == True:
-            param_grid = dict(class_weight = ['balanced', None],\
-                          min_samples_leaf=[0.01, 0.03, 0.05, 5],\
+            param_grid = dict( min_samples_leaf=[0.01, 0.03, 0.05, 5],\
                   max_features=['auto', None],\
                       max_depth = [5, 10, 15],\
                       ccp_alpha = [0.001, 0])
             rf = RandomForestRegressor()
-            grid_searchrf = GridSearchCV(rf, param_grid, scoring = 'balanced_accuracy', \
-                      refit='balanced_accuracy', verbose=0)
+            grid_searchrf = GridSearchCV(rf, param_grid, scoring = 'r2', \
+                      refit='r2', verbose=0)
             grid_searchrf.fit(X_train, y_train.astype(int))
         else:
             rf = RandomForestRegressor() #add parameters later
