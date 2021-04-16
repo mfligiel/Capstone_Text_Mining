c8f6f7bf4d24ffca1b5d4a843ee028e508283a8f
mfligiel <76972796+mfligiel@users.noreply.github.com>
2021-04-16 14:23:20 -0500

    Update __init__.py
    Adding some ways to import without dealing with the submodules.

--- capstone_text_mining/__init__.py
+++ capstone_text_mining/__init__.py
@@ -1,1 +1,8 @@
-
+from .data_clean import data_clean
+from .hand_labelled_features import hand_label
+from .lift import lift
+from .model import model
+from .recode_categorical import recode_categorical
+from .sent_analysis import sent_analysis
+from .spacy_functions import nlp_xtrct_pos, nlp_xtrct_dep, nlp_xtrct_elab, spacyanalysis
+from .topic_model_features import clean_text, topic_features
