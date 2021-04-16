4aa81eaa511c3febff407a98377765d31b98ce65
mfligiel <76972796+mfligiel@users.noreply.github.com>
2021-04-16 10:39:24 -0500

    Create setup.py
    Starting a setup.py will need to add more requirements

---
+++ setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.1.0'
+PACKAGE_NAME = 'capstone_text_mining'
+AUTHOR = 'Matt Fligiel, Carmin Ballou, Zain Jafri'
+AUTHOR_EMAIL = 'matthewfligiel@gmail.com'
+URL = 'https://github.com/mfligiel/Capstone_Text_Mining'
+
+LICENSE = 'Apache License 2.0'
+DESCRIPTION = 'Capstone Text Mining Techniques'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'numpy',
+      'pandas'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      url=URL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages()
+      )
