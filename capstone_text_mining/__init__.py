82777aacc40d6195f49e9e740cf1fb2921fe43b6
mfligiel <76972796+mfligiel@users.noreply.github.com>
2021-04-28 08:22:19 -0500

    Update __init__.py
    adding more fns

--- capstone_text_mining/__init__.py
+++ capstone_text_mining/__init__.py
@@ -1,11 +1,12 @@
 from .data_clean import data_clean
 from .feature_importance import feature_imp
-from .graph_prep import graphprep
+from .graph_prep import graphprep #, clean_text seems to live in graphprep?
 from .graph_viz import graphviz
 from .hand_labelled_features import hand_label
 from .lift import lift
 from .model import model
 from .recode_categorical import recode_categorical
 from .sent_analysis import sent_analysis
+from .shap_visualization import shap_visuals
 from .spacy_functions import nlp_xtrct_pos, nlp_xtrct_dep, nlp_xtrct_elab, spacyanalysis
 from .topic_model_features import clean_text, topic_features
