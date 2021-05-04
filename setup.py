fc577ea6b17ae296c3f7e1519883373d8607b6d1
mfligiel <76972796+mfligiel@users.noreply.github.com>
2021-05-04 16:00:47 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 PACKAGE_NAME = 'capstone_text_mining'
 AUTHOR = 'Matt Fligiel, Carmin Ballou, Zain Jafri'
 AUTHOR_EMAIL = 'matthewfligiel@gmail.com'
